7d9a113ce3c2da7e81235ae1cfb8d6cffe333717
Aabid Ismail <aabid@princeton.edu>
2022-08-08 16:25:34 +0530

    adding initial files correctly

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h1>
+          Wordle
+        </h1>
+      </nav>
+      <Board/>
+      <Keyboard/>
+    </div>  
+  );
+}
+
+export default App;
---
+++ src/Words.js
@@ -0,0 +1,8 @@
+// gonna be available in all places. Will be injected to a context in App.js
+export default defaultBoxes = [
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""]
+]
